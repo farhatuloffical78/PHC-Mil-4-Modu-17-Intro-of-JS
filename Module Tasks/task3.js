b39ee822fc2412ef21cf3de5dd53a8c31804d7ce
farhatuloffical78 <mdfarhatulhaider@gmail.com>
2024-09-29 01:43:56 +0600

    completed task3

---
+++ Module Tasks/task3.js
@@ -0,0 +1,23 @@
+/* Task-3
+You task is to divide the given number by 5 and 
+show the remainder as the output.
+
+Input:
+The first line of the input contains the number.
+
+Output:
+Print the remainder.
+
+Sample Input:
+119
+
+Sample Output:
+4 
+*/
+
+// Practice Problem 3:
+
+givenNumber = 13;
+reminder= 13%5;
+
+console.log(reminder);
